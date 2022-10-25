e740a781ba7e8b27e1291e191af99121d1d70d09
Ian Guedes <82589606+ianvgs@users.noreply.github.com>
2022-10-25 20:29:48 -0300

    Criacao do seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(email:'u1@email', password: '123456')
+
+User.create(email:'u2@email', password: '123456')
+
+
+
+Item.create([{ name: 'ABACAXI PEROLA',weight:1,value:3.5, avaiable: true, durl: "https://firebasestorage.googleapis.com/v0/b/project-482087131936180168.appspot.com/o/cmtimg%2F0000000010443.png?alt=media&token=bfb1ac51-85be-4971-b475-662499671e9b", ean:'tes123tedeean', offer: false,buy_limit: 50, user_id: 1}])
+
+
+
+Item.create([{ name: 'AMEIXA FRESCA',weight:1,value:3.5, avaiable: true, durl: "https://firebasestorage.googleapis.com/v0/b/project-482087131936180168.appspot.com/o/fgopimg%2F0000000001681.png?alt=media&token=07a5b800-41e0-4b31-aa8a-bfd82d974706", ean:'tes123tedeean', offer: true,buy_limit: 100, user_id: 2}])
+
+
+
+
+Item.create([{ name: 'ABACAXI PEROLA',weight:1,value:3.5, avaiable: true, durl: "https://firebasestorage.googleapis.com/v0/b/project-482087131936180168.appspot.com/o/cmtimg%2F0000000010443.png?alt=media&token=bfb1ac51-85be-4971-b475-662499671e9b", ean:'tes123tedeean', offer: false,buy_limit: 50, user_id: 1}])
+
+
+
+Item.create([{ name: 'AMEIXA FRESCA',weight:1,value:3.5, avaiable: true, durl: "https://firebasestorage.googleapis.com/v0/b/project-482087131936180168.appspot.com/o/fgopimg%2F0000000001681.png?alt=media&token=07a5b800-41e0-4b31-aa8a-bfd82d974706", ean:'tes123tedeean', offer: true,buy_limit: 100, user_id: 2}])
+
+
+
+
+Item.create([{ name: 'ABACAXI PEROLA',weight:1,value:3.5, avaiable: true, durl: "https://firebasestorage.googleapis.com/v0/b/project-482087131936180168.appspot.com/o/cmtimg%2F0000000010443.png?alt=media&token=bfb1ac51-85be-4971-b475-662499671e9b", ean:'tes123tedeean', offer: false,buy_limit: 50, user_id: 1}])
+
+
+
+Item.create([{ name: 'AMEIXA FRESCA',weight:1,value:3.5, avaiable: true, durl: "https://firebasestorage.googleapis.com/v0/b/project-482087131936180168.appspot.com/o/fgopimg%2F0000000001681.png?alt=media&token=07a5b800-41e0-4b31-aa8a-bfd82d974706", ean:'tes123tedeean', offer: true,buy_limit: 100, user_id: 2}])
+
+
+
+
