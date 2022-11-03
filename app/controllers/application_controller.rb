a9ed3cf3326b2095db19057c0eb454415b002b42
dianahtech <82589606+ianvgs@users.noreply.github.com>
2022-11-03 20:35:24 -0300

    Verify_authenticity_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
-        #include DeviseTokenAuth::Concerns::SetUserByToken
+        skip_before_action :verify_authenticity_token
+        include DeviseTokenAuth::Concerns::SetUserByToken
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,18 +12,11 @@ Rails.application.routes.draw do
       end
   end 
 
-  #namespace 'api' do
-    #namespace 'v1' do
-    #    resources :natives
-  # end        
-  #end
-
   devise_for :users   
   mount_devise_token_auth_for 'NativeUser', at: 'native'      
   resources :items  
   resources :ordered_item
   get 'home/about'
-
   root 'home#about'
   # For details on the DSL available withithis fil, see https://guides.rubyonrails.org/routing.html
 end
